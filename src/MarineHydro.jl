9e7e31c639b482411646feaee3f767f926a9dd97
Matthieu Ancellin <31126826+mancellin@users.noreply.github.com>
2025-01-21 16:36:55 +0100

    refactor: move generic Green function methods to another file.

--- src/MarineHydro.jl
+++ src/MarineHydro.jl
@@ -17,68 +17,16 @@ using .MathConstants: γ
 using ImplicitAD: implicit_linear
 
 
-abstract type GreensFunction end
-
-"""
-    greens(::GreensFunction, element_1, element_2, wavenumber=nothing)
-
-Calculates the Green's function between two points.
-
-# Arguments
-- `element_1`: first point, as a `NamedTuple` with the field `center`.
-- `element_2`: second point, as a `NamedTuple` with the field `center`.
-- `wavenumber`: Defines the wavenumber for waves ~~~~~. Optional for some Greens functions such as `Rankine()`.
-"""
-function greens end
-
-"""
-    gradient_greens(::GreensFunction, element_1, element_2, wavenumber; with_respect_to_first_variable=false)
-
-Calculates the gradient of the Rankine Green's function between two points.
-
-# Arguments
-- `element_1`: first point, as a `NamedTuple` with the field `center`.
-- `element_2`: second point, as a `NamedTuple` with the field `center`.
-- `wavenumber`: Defines the wavenumber for waves ~~~~~. Optional for some Greens functions such as `Rankine()`.
-- `with_respect_to_first_variable`: A boolean flag (default is `false`). If `true`, computes the gradient with respect to `element_1`; otherwise, computes the gradient with respect to `element_2`.
-"""
-function gradient_greens end
-
-"""
-    integral(::GreensFunction, element_1, element_2, wavenumber)
-
-Calculates the integral of Green's function over a panel.
-
-# Arguments
-- `element_1`: first point, as a `NamedTuple` with the field `center`.
-- `element_2`: integration panel, as a `NamedTuple` with the fields `center`, `normal`, `vertices`, `radius`, `normal` and `area`.
-- `wavenumber`: Defines the wavenumber for waves ~~~~~. Optional for some Greens functions such as `Rankine()`.
-"""
-function integral end
-
-"""
-    integral_gradient(::GreensFunction, element_1, element_2, wavenumber; with_respect_to_first_variable=false)
-
-Calculates the integral of the gradient of Green's function over a panel.
-
-# Arguments
-- `element_1`: first point, as a `NamedTuple` with the field `center`.
-- `element_2`: integration panel, as a `NamedTuple` with the fields `center`, `normal`, `vertices`, `radius`, `normal` and `area`.
-- `wavenumber`: Defines the wavenumber for waves ~~~~~. Optional for some Greens functions such as `Rankine()`.
-- `with_respect_to_first_variable`: A boolean flag (default is `false`). If `true`, computes the gradient with respect to `element_1`; otherwise, computes the gradient with respect to `element_2`.
-"""
-function integral_gradient end
-
 const τ̅ = 2π
 
 include("Meshes.jl") #capytaine one
+include("green_functions/abstract_greens_function.jl")
 include("green_functions/rankine.jl")
 include("green_functions/rankine_reflected.jl")
 include("green_functions/rankine_reflected_negative.jl")
 include("green_functions/struve.jl")
 include("green_functions/wu_.jl")
 include("green_functions/exact_Guevel_Delhommeau.jl")
-
 
 
 """
--- src/green_functions/abstract_greens_function.jl
+++ src/green_functions/abstract_greens_function.jl
@@ -1,24 +1,52 @@
-
 abstract type GreensFunction end
 
+"""
+    greens(::GreensFunction, element_1, element_2, wavenumber=nothing)
+
+Calculates the Green's function between two points.
+
+# Arguments
+- `element_1`: first point, as a `NamedTuple` with the field `center`.
+- `element_2`: second point, as a `NamedTuple` with the field `center`.
+- `wavenumber`: Defines the wavenumber for waves ~~~~~. Optional for some Greens functions such as `Rankine()`.
+"""
 function greens end
+
+"""
+    gradient_greens(::GreensFunction, element_1, element_2, wavenumber; with_respect_to_first_variable=false)
+
+Calculates the gradient of the Rankine Green's function between two points.
+
+# Arguments
+- `element_1`: first point, as a `NamedTuple` with the field `center`.
+- `element_2`: second point, as a `NamedTuple` with the field `center`.
+- `wavenumber`: Defines the wavenumber for waves ~~~~~. Optional for some Greens functions such as `Rankine()`.
+- `with_respect_to_first_variable`: A boolean flag (default is `false`). If `true`, computes the gradient with respect to `element_1`; otherwise, computes the gradient with respect to `element_2`.
+"""
 function gradient_greens end
+
+"""
+    integral(::GreensFunction, element_1, element_2, wavenumber)
+
+Calculates the integral of Green's function over a panel.
+
+# Arguments
+- `element_1`: first point, as a `NamedTuple` with the field `center`.
+- `element_2`: integration panel, as a `NamedTuple` with the fields `center`, `normal`, `vertices`, `radius`, `normal` and `area`.
+- `wavenumber`: Defines the wavenumber for waves ~~~~~. Optional for some Greens functions such as `Rankine()`.
+"""
 function integral end
+
+"""
+    integral_gradient(::GreensFunction, element_1, element_2, wavenumber; with_respect_to_first_variable=false)
+
+Calculates the integral of the gradient of Green's function over a panel.
+
+# Arguments
+- `element_1`: first point, as a `NamedTuple` with the field `center`.
+- `element_2`: integration panel, as a `NamedTuple` with the fields `center`, `normal`, `vertices`, `radius`, `normal` and `area`.
+- `wavenumber`: Defines the wavenumber for waves ~~~~~. Optional for some Greens functions such as `Rankine()`.
+- `with_respect_to_first_variable`: A boolean flag (default is `false`). If `true`, computes the gradient with respect to `element_1`; otherwise, computes the gradient with respect to `element_2`.
+"""
 function integral_gradient end
 
-
-function greens(gf_tuple::NTuple{N, GreensFunction} where N, element_1, element_2, wavenumber)
-    return sum(Complex(greens(gf, element_1, element_2, wavenumber)) for gf in gf_tuple)
-end
-
-function gradient_greens(gf_tuple::NTuple{N, GreensFunction} where N, element_1, element_2, wavenumber; with_respect_to_first_variable=false)
-    return sum(Vector{ComplexF64}(gradient_greens(gf, element_1, element_2, wavenumber; with_respect_to_first_variable)) for gf in gf_tuple)
-end
-
-function integral(gf_tuple::NTuple{N, GreensFunction} where N, element_1, element_2, wavenumber)
-    return sum(Complex(integral(gf, element_1, element_2, wavenumber)) for gf in gf_tuple)
-end
-
-function integral_gradient(gf_tuple::NTuple{N, GreensFunction} where N, element_1, element_2, wavenumber; with_respect_to_first_variable=false)
-    return sum(Vector{ComplexF64}(integral_gradient(gf, element_1, element_2, wavenumber; with_respect_to_first_variable)) for gf in gf_tuple)
-end
