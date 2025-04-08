af61abdcd67a222138fea933cd2ecb4f6ec80a9d
Matthieu Ancellin <31126826+mancellin@users.noreply.github.com>
2025-04-08 12:06:26 +0200

    refactor: reorder meshes.jl

--- src/meshes.jl
+++ src/meshes.jl
@@ -1,4 +1,6 @@
-using PyCall
+############
+#  Meshes  #
+############
 
 struct Mesh
     vertices::AbstractMatrix
@@ -27,7 +29,30 @@ struct Mesh
     end
 end
 
+struct StaticArraysMesh
+    vertices::Vector{SVector{3, Float64}}
+    faces::Vector{SVector{4, Int64}}
+    centers::Vector{SVector{3, Float64}}
+    normals::Vector{SVector{3, Float64}}
+    areas::Vector{Float64}
+    radii::Vector{Float64}
+    nvertices::Int64
+    nfaces::Int64
+end
 
+###################################
+#  Loading meshes from Capytaine  #
+###################################
+
+
+using PyCall
+
+function example_mesh_from_capytaine()
+    cpt = pyimport("capytaine")
+    radius = 1.0
+    resolution = (4, 4)
+    return cpt.mesh_sphere(radius=radius, center=(0, 0, 0), resolution=resolution, name="floating sphere").immersed_part()
+end
 
 function Mesh(mesh::PyObject)
     mesh = Mesh(
@@ -37,18 +62,26 @@ function Mesh(mesh::PyObject)
     return mesh
 end
 
-
-function element(mesh::Mesh, J::Int)
-    return (
-        center = mesh.centers[J, :],
-        vertices = mesh.vertices[mesh.faces[J, :] .+ 1, :],
-        radius = mesh.radii[J],
-        normal = mesh.normals[J, :],
-        area = mesh.areas[J],
-        faces = mesh.faces[J, :] .+ 1
+function StaticArraysMesh(mesh::PyObject)
+    mesh = StaticArraysMesh(
+        [SVector(v[1], v[2], v[3]) for v in eachrow(mesh.vertices)],
+        [SVector(f[1], f[2], f[3], f[4]) .+ 1 for f in eachrow(mesh.faces)],
+        [SVector(c[1], c[2], c[3]) for c in eachrow(mesh.faces_centers)],
+        [SVector(n[1], n[2], n[3]) for n in eachrow(mesh.faces_normals)],
+        mesh.faces_areas,
+        mesh.faces_radiuses,
+        mesh.nb_vertices,
+        mesh.nb_faces
     )
+    return mesh
 end
 
+
+##############
+#  Elements  #
+##############
+
+# Mesh elements can be provided individually as NamedTuples, for instance to test the Green's function
 center(nt::NamedTuple) = nt[:center]
 vertices(nt::NamedTuple) = nt[:vertices]
 radius(nt::NamedTuple) = nt[:radius]
@@ -57,9 +90,14 @@ area(nt::NamedTuple) = nt[:area]
 faces(nt::NamedTuple) = nt[:faces]
 
 
+# When using a Mesh, the element is stored as a reference to the Mesh and a index
 struct LazyElement
     mesh::Mesh
     index::Integer
+end
+
+function element(mesh::Mesh, J::Int)
+    return LazyElement(mesh, J)
 end
 
 center(e::LazyElement) = e.mesh.centers[e.index, :] #, e.mesh.centers[e.index, 1], e.mesh.centers[e.index, 2]
@@ -68,6 +106,36 @@ area(e::LazyElement) = e.mesh.areas[e.index]
 radius(e::LazyElement) = e.mesh.radii[e.index]
 vertices(e::LazyElement) = e.mesh.vertices[e.mesh.faces[e.index, :] .+ 1, :]
 
+
+# When using a StaticArraysMesh, the element is stored as a set of static arrays (although LazyElement might also work)
+struct StaticElement
+    center::SVector{3, Float64}
+    vertices::SMatrix{4, 3, Float64, 12}
+    normal::SVector{3, Float64}
+    area::Float64
+    radius::Float64
+end
+
+function element(mesh::StaticArraysMesh, J::Int)
+    return StaticElement(
+        mesh.centers[J],
+        hcat(mesh.vertices[mesh.faces[J]]...)',  # makes a 4x3 SMatrix
+        mesh.normals[J],
+        mesh.areas[J],
+        mesh.radii[J],
+    )
+end
+
+center(e::StaticElement) = e.center
+normal(e::StaticElement) = e.normal
+area(e::StaticElement) = e.area
+radius(e::StaticElement) = e.radius
+vertices(e::StaticElement) = e.vertices
+
+
+########################
+#  Reflected elements  #
+########################
 
 struct ReflectedElement{T}
     element::T
@@ -89,58 +157,6 @@ function vertices(e::ReflectedElement{T} where T <: Union{NamedTuple, LazyElemen
         ]   # Inverting order such that order is still consistent with normal vector
 end
 
-
-using StaticArrays
-
-struct StaticArraysMesh
-    vertices::Vector{SVector{3, Float64}}
-    faces::Vector{SVector{4, Int64}}
-    centers::Vector{SVector{3, Float64}}
-    normals::Vector{SVector{3, Float64}}
-    areas::Vector{Float64}
-    radii::Vector{Float64}
-    nvertices::Int64
-    nfaces::Int64
-end
-
-function StaticArraysMesh(mesh::PyObject)
-    mesh = StaticArraysMesh(
-        [SVector(v[1], v[2], v[3]) for v in eachrow(mesh.vertices)],
-        [SVector(f[1], f[2], f[3], f[4]) .+ 1 for f in eachrow(mesh.faces)],
-        [SVector(c[1], c[2], c[3]) for c in eachrow(mesh.faces_centers)],
-        [SVector(n[1], n[2], n[3]) for n in eachrow(mesh.faces_normals)],
-        mesh.faces_areas,
-        mesh.faces_radiuses,
-        mesh.nb_vertices,
-        mesh.nb_faces
-    )
-    return mesh
-end
-
-struct StaticElement
-    center::SVector{3, Float64}
-    vertices::SMatrix{4, 3, Float64, 12}
-    normal::SVector{3, Float64}
-    area::Float64
-    radius::Float64
-end
-
-function MarineHydro.element(mesh::StaticArraysMesh, J::Int)
-    return StaticElement(
-        mesh.centers[J],
-        hcat(mesh.vertices[mesh.faces[J]]...)',  # makes a 4x3 SMatrix
-        mesh.normals[J],
-        mesh.areas[J],
-        mesh.radii[J],
-    )
-end
-
-center(e::StaticElement) = e.center
-normal(e::StaticElement) = e.normal
-area(e::StaticElement) = e.area
-radius(e::StaticElement) = e.radius
-vertices(e::StaticElement) = e.vertices
-
 center(e::ReflectedElement{StaticElement}) = (c = center(e.element); SVector(c[1], c[2], -c[3]))
 normal(e::ReflectedElement{StaticElement}) = (n = normal(e.element); SVector(n[1], n[2], -n[3]))
 area(e::ReflectedElement{StaticElement}) = area(e.element)
@@ -155,10 +171,3 @@ function vertices(e::ReflectedElement{StaticElement})
     ]   # Inverting order such that order is still consistent with normal vector
 end
 
-
-function example_mesh_from_capytaine()
-    cpt = pyimport("capytaine")
-    radius = 1.0
-    resolution = (4, 4)
-    return cpt.mesh_sphere(radius=radius, center=(0, 0, 0), resolution=resolution, name="floating sphere").immersed_part()
-end
